6f3b5cf25b885f13e8c12309e0adb776d0c6df9b
q <q@q.com>
2024-09-11 17:05:12 +0800

    fix footer and config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,8 +9,8 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.mypzh.com' },
-    // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-    { title: '访问文档中心获取更多帮助', url: 'https://blog.mypzh.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.mypzh.com' }
+    // { title: '访问文档中心获取更多帮助', url: 'https://blog.mypzh.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -21,8 +21,8 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'blog.mypzh.COM',
-  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_3: 'blog.mypzh.com',
+  // HEO_HERO_TITLE_4: '新版上线',
   // HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://blog.mypzh.com',
 
@@ -33,7 +33,8 @@ const CONFIG = {
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  // HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
@@ -115,7 +116,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
