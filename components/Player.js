9d23dfadb2d831cdab582c228fe096c208c24ddf
q <q@q.com>
2024-09-19 17:04:54 +0800

    fix

--- components/Player.js
+++ components/Player.js
@@ -21,7 +21,9 @@ const Player = () => {
   const musicMetingEnable = siteConfig('MUSIC_PLAYER_METING')
   const musicMetingCDNUrl = siteConfig(
     'MUSIC_PLAYER_METING_CDN_URL',
-    'https://cdnjs.cloudflare.com/ajax/libs/meting/2.0.1/Meting.min.js'
+    // 'https://cdnjs.cloudflare.com/ajax/libs/meting/2.0.1/Meting.min.js'
+    // 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js'
+    'https://cdn.jsdelivr.net/npm/meting@2.0.1/dist/Meting.min.js'
   )
 
   const initMusicPlayer = async () => {
